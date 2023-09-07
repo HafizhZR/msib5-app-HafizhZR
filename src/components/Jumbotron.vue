0a9c9a9b965a299b0e6413d9e7ad9b42e7f150c2
HafizhZR <hafizhzaghlul882@gmail.com>
2023-09-07 12:50:33 +0700

    fix: responsive

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="lg:flex lg:justify-center lg:items-center flex flex-col lg:flex-row">
-    <div class="lg:flex-col lg:w-2/5 lg:m-24 order-2 lg:order-1">
-      <h1 class="text-3xl lg:text-6xl font-semibold mb-10 text-center lg:text-left">Welcome to GITS Commerce</h1>
-      <p class="text-md lg:text-xl w-full lg:w-5/6 mb-6 lg:mb-10 text-center lg:text-left ">Morpheme is a next-gen
-        financial technology company in the process of reinventing
-        banking.</p>
-      <button class="bg-[#0984DD] px-[18px] py-[10px] rounded-lg text-white flex mx-auto lg:mx-0"><img
-          src="../assets/IconCart.png" alt="logo" class="w-6 h-6 mr-2">Start Shopping</button>
-    </div>
-    <div class="w-max-3/5 order-1 lg:order-2">
-      <img class="w-max-[100px] h-max-[100px]" src="../assets/Jumbotron.png" alt="logo cart">
+  <div class="flex mx-auto">
+    <div class="lg:flex lg:justify-center lg:items-center flex flex-col mx-auto lg:flex-row">
+      <div class="lg:flex-col flex flex-col mx-auto lg:w-2/5 lg:m-24 order-2 lg:order-1">
+        <h1 class="text-3xl lg:text-6xl font-semibold mb-10 text-center lg:text-left">Welcome to GITS Commerce</h1>
+        <p class="text-md lg:text-xl w-3/4 lg:w-5/6 mb-6 lg:mb-10 flex mx-auto lg:mx-0 text-center lg:text-left ">Morpheme is a next-gen
+          financial technology company in the process of reinventing
+          banking.</p>
+        <button class="bg-[#0984DD] px-[18px] py-[10px] rounded-lg text-white flex mx-auto lg:mx-0 w-fit"><img
+            src="../assets/IconCart.png" alt="logo" class="w-6 h-6 mr-2">Start Shopping</button>
+      </div>
+      <div class="order-1 lg:order-2 flex mx-auto">
+        <img class="max-w-[400px] max-h-[400px] md:max-w-[600px] md:max-h-[600px] xl:max-w-[800px] xl:max-h-[800px]" src="../assets/Jumbotron.png" alt="logo cart">
+      </div>
     </div>
   </div>
 </template>
